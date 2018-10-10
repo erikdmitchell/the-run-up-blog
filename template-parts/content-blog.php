bffd7411aaa52bf5af6cb622229a6f9135f37dd6
Erik <erikdmitchell@gmail.com>
2018-10-10 16:01:00 -0400

    removed unused styles and templates

--- template-parts/content-blog.php
+++
@@ -1,8 +0,0 @@
-<?php
-$paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
-
-if ( 1 === $paged ) :
-    get_template_part( 'template-parts/content-blog', 'landing' );
-else :
-    get_template_part( 'template-parts/content-blog', 'archive' );
-endif;
