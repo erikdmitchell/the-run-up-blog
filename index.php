61a5b676669c1392470ac1ba0b54683de3926ce6
Erik <erikdmitchell@gmail.com>
2018-10-10 12:28:12 -0400

    updated main page

--- index.php
+++ index.php
@@ -9,34 +9,8 @@
 get_header(); ?>
 
 <div class="container">
-    <div class="row">
-        <div class="col-md-8">
-            <?php
-            while ( have_posts() ) :
-                the_post();
-                ?>
-                <?php get_template_part( 'content' ); ?>
-                <?php
-                // If comments are open or we have at least one comment, load up the comment template.
-                if ( comments_open() || get_comments_number() ) {
-                    comments_template();
-                }
-                ?>
-            <?php endwhile; ?>
-            
-            <?php
-                the_posts_navigation(
-                    array(
-                        'prev_text' => __( '&laquo; Older Posts', 'tru-blog' ),
-                        'next_text' => __( 'Newer Posts &raquo;', 'tru-blog' ),
-                    )
-                );
-                ?>
-            
-        </div>
-        <div class="col-md-4">
-            <?php get_sidebar(); ?>
-        </div>
+    <div class="row content">
+        <?php get_template_part( 'template-parts/content', 'blog' ); ?>
     </div>
 </div><!-- .container -->
 
