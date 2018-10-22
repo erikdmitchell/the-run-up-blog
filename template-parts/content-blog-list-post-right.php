cc58851b3a930f349f232808d6849e98485888b0
Erik <erikdmitchell@gmail.com>
2018-10-22 10:42:30 -0400

    added post on date

--- template-parts/content-blog-list-post-right.php
+++ template-parts/content-blog-list-post-right.php
@@ -3,3 +3,7 @@
 <div class="title">
     <a href="<?php the_permalink(); ?>"><?php the_title( '<h2 class="blog-post-right">', '</h2>' ); ?></a>
 </div>
+
+<div class="entry-meta">
+    <?php tru_blog_theme_posted_on(); ?>
+</div><!-- .entry-meta -->
--- template-parts/content-blog-list-post.php
+++ template-parts/content-blog-list-post.php
@@ -4,6 +4,10 @@
     <a href="<?php the_permalink(); ?>"><?php the_title( '<h2 class="blog-list-post">', '</h2>' ); ?></a>
 </div>
 
+<div class="entry-meta">
+    <?php tru_blog_theme_posted_on(); ?>
+</div><!-- .entry-meta -->
+
 <div class="excerpt">
     <?php echo tru_blog_excerpt_by_id( get_the_ID(), 30, '', '<a href="' . get_permalink( get_the_ID() ) . '">...more</a>' ); ?>
 </div>
